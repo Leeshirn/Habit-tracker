fad206939dca61f57b28df874771ef72823c8d02
Asachi <leonaasachi@gmail.com>
2024-03-15 11:43:26 +0300

    A personal habit tracker for self development

--- habit/main/views.py
+++ habit/main/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login, logout
+from django.contrib import messages
 
 # Create your views here.
 
-def index(response):
-  return HttpResponse("<h1>Habit Tracker</h1>")
+def home(request,):
+  #Check to see if logging in 
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    #Authenticate
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request,user)
+      return redirect('home')
+    else:
+      messages.success(request,"There was an error logging in")
+      return redirect('home')
+  else:
+       return render(request, 'home.html', {})
+
+def logout_user(request):
+  pass
